c795c65e50d93d53be92f0631ddf49bdda9df986
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-11-07 18:32:02 +0100

    Products Detail component
    nx g @nx/angular:component detail --directory=products/src/app/components/detail

--- products/src/app/app.module.ts
+++ products/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DetailComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(

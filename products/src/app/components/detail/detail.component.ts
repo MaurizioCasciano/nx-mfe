e59f1e22d27762de585fe753a1965027eeb4255c
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-11-07 23:42:21 +0100

    APP_BASE_HREF="products"

--- products/src/app/components/detail/detail.component.ts
+++ products/src/app/components/detail/detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import {APP_BASE_HREF} from "@angular/common";
 
 @Component({
   selector: 'nx-mfe-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss'],
+  styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {}
+export class DetailComponent {
+  public baseHref = inject(APP_BASE_HREF)
+}
--- products/src/app/remote-entry/entry.module.ts
+++ products/src/app/remote-entry/entry.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {APP_BASE_HREF, CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
-import { RemoteEntryComponent } from './entry.component';
-import { remoteRoutes } from './entry.routes';
+import {RemoteEntryComponent} from './entry.component';
+import {remoteRoutes} from './entry.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {DetailComponent} from "../components/detail/detail.component";
 
 @NgModule({
   declarations: [RemoteEntryComponent, DetailComponent],
   imports: [CommonModule, RouterModule.forChild(remoteRoutes)],
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(), {provide: APP_BASE_HREF, useValue: "products"}],
 })
-export class RemoteEntryModule {}
+export class RemoteEntryModule {
+}
